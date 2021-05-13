0abca33883032d71ffdef3577ce54f490e96a1a5
qihan-z <69850024+qihan-z@users.noreply.github.com>
2021-05-13 18:30:51 +0800

    Add files via upload

---
+++ Git test/Git_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  Git_testApp.swift
+//  Git test
+//
+//  Created by QiHan Zhao on 2021/5/13.
+//
+
+import SwiftUI
+
+@main
+struct Git_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
